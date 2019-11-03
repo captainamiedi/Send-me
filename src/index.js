fd5b082cb36d8e60ea38816c17acaa0aa5432637
captainamiedi <captainamiedi1@gmail.com>
2019-11-03 08:35:05 +0100

    environment set

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import cors from 'cors';
+import bodyParser from 'body-parser';
 import 'dotenv/config';
 
 
 
 const app = express();
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
